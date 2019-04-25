e5964e8f31f37bf7e3df6354c9d93f82be8974c7
Vbobell <vinicius.bobel41@gmail.com>
2019-04-25 09:27:40 -0300

    Separete gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 const gulp = require('gulp');
 
-gulp.task('copy-files', async () => {
-    await gulp.src(['./static/css', './static/images', './static/js'])
-          .pipe(gulp.dest('./build/static'))
+gulp.task('copy-css', () => {
+    return gulp.src(['./static/css/*'])
+          .pipe(gulp.dest('./build/static/css'))
+});
 
-          return gulp.src(['./static/index.html'])
+gulp.task('copy-js', () => {
+    return gulp.src(['./static/js/*'])
+          .pipe(gulp.dest('./build/static/js'))
+});
+
+gulp.task('copy-images', () => {
+    return gulp.src(['./static/images/*'])
+          .pipe(gulp.dest('./build/static/images'))
+});
+
+gulp.task('copy-index', () => {
+    return gulp.src(['./static/index.html'])
           .pipe(gulp.dest('./build'))
 });
 
-gulp.task('default', gulp.series(['copy-files']));
+gulp.task('default', gulp.series(['copy-css', 'copy-js', 'copy-images', 'copy-index']));
