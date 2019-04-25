a6eca4c7777ba950dd35878cff19b46dc24ff573
Vbobell <vinicius.bobel41@gmail.com>
2019-04-25 09:20:21 -0300

    Change gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 
-gulp.task('copy-files', () => {
-    return gulp.src(['./static/css', './static/images', './static/js', './static/index.html'])
-   .pipe(gulp.dest('build'));
+gulp.task('copy-files', async () => {
+    await gulp.src(['./static/css', './static/images', './static/js'])
+          .pipe(gulp.dest('./build/static'))
+
+          return gulp.src(['./static/index.html'])
+          .pipe(gulp.dest('./build'))
 });
 
 gulp.task('default', gulp.series(['copy-files']));
